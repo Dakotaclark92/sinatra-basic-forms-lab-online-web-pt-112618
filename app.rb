bbc30f2df504489f5dfde7b75667c742af67da76
Dakota Clark <dakotaclark92@yahoo.com>
2019-03-31 02:06:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,13 +6,7 @@ class App < Sinatra::Base
     erb :index
   end
   
-  post '/create_puppy' do
-    cp = Puppy.new
-    @name = cp.puppies(params[:name])
-    @breed = cp.puppies(params[:breed])
-    @age = cp.puppies(params[:age])
-    
-    
+  post '/create' do
     erb :create_puppy
   end
   
