b7996125c8467f722c5deda578a56c542e9d98b1
Dakota Clark <dakotaclark92@yahoo.com>
2019-03-31 02:06:20 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,14 +1,10 @@
 class Puppy
-  attr_accessor (name, breed, age)
+  attr_accessor :name, :breed, :months_old
   
-  def initialize
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @months_old = months_old
   end
     
-  
-  
-  
-  
 end
